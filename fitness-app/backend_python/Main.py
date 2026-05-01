d72e3ce98c9bea68d0b40cc6a8c52436c9bf9d43
SuryaNandan07 <gvsnandan07@gmail.com>
2026-05-01 21:05:33 +0530

    Backend first commit

--- fitness-app/backend_python/Main.py
+++ fitness-app/backend_python/Main.py
@@ -1,1 +1,11 @@
-print(3)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/exercises")
+def get_exercises():
+    return ["Pushup", "Squat", "Plank"]
+
+@app.get("/")
+def home():
+    return {"message": "Backend running"}
