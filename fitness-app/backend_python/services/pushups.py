686d9319a23ee210e41edf55f9958a0094c8b565
SuryaNandan07 <gvsnandan07@gmail.com>
2026-05-03 17:41:54 +0530

    Lunges

--- fitness-app/backend_python/services/pushups.py
+++ fitness-app/backend_python/services/pushups.py
@@ -8,8 +8,8 @@ import math
 model = YOLO("yolov8s-pose.pt")
 
 # VIDEO / CAMERA
-#cap = cv2.VideoCapture("male-Bodyweight-push-up-side.mp4")
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture("videos/male-Bodyweight-push-up-side.mp4")
+#cap = cv2.VideoCapture(1)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
