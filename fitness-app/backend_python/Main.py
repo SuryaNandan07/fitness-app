d05bc38f635b84617dc2fcd53b4fe0daead5ed5f
SuryaNandan07 <gvsnandan07@gmail.com>
2026-05-07 07:56:25 +0530

    Skeleton placed

--- fitness-app/backend_python/Main.py
+++ fitness-app/backend_python/Main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 import threading
 from services import squarts  # start with ONE exercise first
+from pydantic import BaseModel
+import base64
+import cv2
+import numpy as np
 
 app = FastAPI()
-
+class FrameRequest(BaseModel):
+    image: str
 data = {
     "reps": 0,
     "stage": "up",
-    "feedback": "Ready"
+    "feedback": "Ready",
+    "frame": ""
 }
 
 running = False
@@ -39,3 +45,16 @@ def stop():
 @app.get("/status")
 def status():
     return data
+
+
+@app.post("/process-frame")
+def process_frame(req: FrameRequest):
+    image_data = req.image.split(",")[-1]
+    img_bytes = base64.b64decode(image_data)
+
+    np_arr = np.frombuffer(img_bytes, np.uint8)
+    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    result = squarts.process_frame(frame, data)
+
+    return result
